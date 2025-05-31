5235ab235cfc52e8522010d2452469d40db052a6
Jana Nekvapilová <jana.nekva@gmail.com>
2025-05-31 11:09:05 +0200

    oprava class na className

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -7,32 +7,32 @@ export const HomePage = () => {
 
   return (
     <div className="container">
-      <div class="joke">
-        <div class="joke__body">
-          <div class="joke__user">
-            <img class="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
-            <p class="user-name">Neroxx</p>
+      <div className="joke">
+        <div className="joke__body">
+          <div className="joke__user">
+            <img className="user-avatar" src="https://raw.githubusercontent.com/Czechitas-podklady-WEB/dadjokes/main/users/user01.png" />
+            <p className="user-name">Neroxx</p>
           </div>
 
-          <p class="joke__text">
+          <p className="joke__text">
             The secret service isn't allowed to yell "Get down!" anymore when
             the president is about to be attacked. Now they have to yell
             "Donald, duck!"
           </p>
         </div>
-        <div class="joke__likes">
+        <div className="joke__likes">
           <button 
           id="btn-up" 
-          class="btn-like btn-like--up"
+          className="btn-like btn-like--up"
           onClick={() => setUpLikes(upLikes + 1)}
           ></button>
-          <span id="likes-up" class="likes-count likes-count--up">{upLikes}</span>
+          <span id="likes-up" className="likes-count likes-count--up">{upLikes}</span>
           <button
           id="btn-down" 
-          class="btn-like btn-like--down"
+          className="btn-like btn-like--down"
           onClick={() => setDownLikes(downLikes +1)}
           ></button>
-          <span id="likes-down" class="likes-count likes-count--down">{downLikes}</span>
+          <span id="likes-down" className="likes-count likes-count--down">{downLikes}</span>
         </div>
       </div>
     </div>
